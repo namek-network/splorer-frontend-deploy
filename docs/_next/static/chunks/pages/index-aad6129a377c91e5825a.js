4c2bcb61d39f598998682caa9950013c84eede58
gavfu <gavfu@outlook.com>
2021-03-09 09:48:28 +0000

    Automated release: 2021-03-09, 09:48:28

---
+++ docs/_next/static/chunks/pages/index-aad6129a377c91e5825a.js
@@ -0,0 +1,1 @@
+_N_E=(window.webpackJsonp_N_E=window.webpackJsonp_N_E||[]).push([[17],{"/EDR":function(n,t,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return u("23aj")}])},"23aj":function(n,t,u){"use strict";u.r(t),u.d(t,"default",(function(){return i}));var _=u("nKUr"),o=u("01WV");function i(){return Object(_.jsx)(o.default,{})}}},[["/EDR",1,2,3,4,8,9,6,0,5,7]]]);
